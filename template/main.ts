c4bf9312c035bcb6b822efa1ec43e1d8999fe978
Jan Wilmake <jan@wilmake.com>
2025-06-07 16:43:42 +0200

    wip

--- template/main.ts
+++ template/main.ts
@@ -2,19 +2,24 @@ import { withStripeflare, StripeUser, DORM } from "stripeflare";
 export { DORM };
 
 // StripeUser can be extended
-export default withStripeflare<StripeUser>({
-  // customMigrations: optional way to overwrite default user table with an extension to your database
-  // version: "1" // resets data
-  handler: {
-    async fetch(request, env, ctx): Promise<Response> {
-      const t = Date.now();
-      const { charged, message } = await ctx.charge(1, false);
-      const speed = Date.now() - t;
-      return new Response(
-        charged
-          ? `Charged ${ctx.user.name} 1 cent in ${speed}ms`
-          : `Could not charge user in ${speed}ms`,
-      );
-    },
-  },
-});
+export default {
+  fetch: withStripeflare<StripeUser>(async (request, env, ctx) => {
+    const t = Date.now();
+    if (!ctx.registered || ctx.user.balance <= 0) {
+      return new Response("User should pay at " + ctx.paymentLink, {
+        status: 402,
+        headers: { Location: ctx.paymentLink },
+      });
+    }
+
+    console.log({ user: ctx.user });
+
+    const { charged, message } = await ctx.charge(1, false);
+    const speed = Date.now() - t;
+    return new Response(
+      charged
+        ? `Charged ${ctx.user.name} 1 cent in ${speed}ms`
+        : `Could not charge user in ${speed}ms`,
+    );
+  }),
+};
